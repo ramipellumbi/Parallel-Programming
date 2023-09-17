38fcca56e1840fb20c9f859db3bd6035ac66938f
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2023-09-17 00:20:00 -0400

    Fix comment

--- a1/part1/integrate.h
+++ a1/part1/integrate.h
@@ -1,22 +1,21 @@
 #ifndef INTEGRATE_H
 #define INTEGRATE_H
 
+// Have the interval:
+//      |--------X--------|--------X--------|  ...   |--------X--------|
+//      0      1/(2N)    1/N      3/(2N)   2/N   (N-1)/N   (2N-1)/2N  N/N
+//
+// Where the:
+//   - | are the endpoints that are distance 1/N apart
+//   - X are the midpoints that are distance 1/N apart.
+//
+// 0 can represent any arbitrary start and 1 can represent any arbitrary end.
 /**
- * Have the interval:
- *      |--------X--------|--------X--------|  ...   |--------X--------|
- *      0      1/(2N)    1/N      3/(2N)   2/N   (N-1)/N   (2N-1)/2N  N/N
- *
- * Where the:
- *   - | are the endpoints that are distance 1/N apart 
- *   - X are the midpoints that are distance 1/N apart.
- * 
- * 0 can represent any arbitrary start and 1 can represent any arbitrary end.
- * 
  * @param start The start of the interval to integrate over
  * @param end The end of the interval to integrate over
  * @param num_steps The number of steps to use in the approximation
  * @param func The function to integrate
- * 
+ *
  * @return A double with the value of the integration result
  */
 double integrate_midpoint_rule(double start, double end, double num_steps, double (*func)(double));
