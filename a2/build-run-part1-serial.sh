c416fef971582bd64a67fdc68b393bcd1dde06b7
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2023-09-24 20:09:17 -0400

    a2 template

---
+++ a2/build-run-part1-serial.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=pi
+#SBATCH --ntasks=1 --nodes=1 --cpus-per-task=1
+#SBATCH --mem-per-cpu=7G
+#SBATCH --time=10:00
+#SBATCH --partition=day
+#SBATCH --reservation=cpsc424
+#SBATCH --output=pi.out-%j
+
+module load intel
+pwd
+echo $SLURMD_NODENAME
+make clean
+make serial
+time serial
+time serial
+time serial
