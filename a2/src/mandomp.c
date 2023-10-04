64a6fff2781ceba6d18e662baf09deff2aebdb34
ramipellumbi <ramipellumbi@gmail.com>
2023-10-04 12:40:58 -0400

    Add mandomp-avx

--- a2/src/mandomp.c
+++ a2/src/mandomp.c
@@ -7,6 +7,9 @@
 #include "timing.h"
 #include "utilities.h"
 
+static const size_t NUM_X_ITERATIONS = 2500;
+static const size_t NUM_Y_ITERATIONS = 1250;
+static const size_t MAX_ITERATIONS = 25000;
 static const double CELL_SIDE_LENGTH = 0.001;
 
 int main(int argc, char *argv[])
@@ -27,36 +30,37 @@ int main(int argc, char *argv[])
 
     // set seed
     unsigned seed = 144545;
+    dsrand(seed);
 
     // initialize timing measures
     double start_wc_time = 0.0, end_wc_time = 0.0;
     double start_cpu_time = 0.0, end_cpu_time = 0.0;
 
     int number_of_cells_inside_mandelbrot_set = 0, total_iterations = 0;
-    int max_x = (int)(2.5 / CELL_SIDE_LENGTH);
-    int max_y = (int)(1.25 / CELL_SIDE_LENGTH);
+    int number_of_cells_inside_mandelbrot_set_th, total_iterations_th;
 
     // start the timing
     timing(&start_wc_time, &start_cpu_time);
-#pragma omp parallel shared(number_of_cells_inside_mandelbrot_set, total_iterations) default(none)
+#pragma omp parallel shared(number_of_cells_inside_mandelbrot_set, total_iterations) private(number_of_cells_inside_mandelbrot_set_th, total_iterations_th) default(none)
     {
-        dsrand(seed);
-        int number_of_cells_inside_mandelbrot_set_th = 0, total_iterations_th = 0;
+        
+        number_of_cells_inside_mandelbrot_set_th = 0; 
+        total_iterations_th = 0;
 
 #pragma omp for
-        for (size_t i = 0; i <= max_x; ++i)
+        for (size_t i = 0; i < NUM_X_ITERATIONS; ++i)
         {
             double current_bottom_left_x = -2.0 + i * CELL_SIDE_LENGTH;
             double cell_max_x = current_bottom_left_x + CELL_SIDE_LENGTH;
 
-            for (size_t j = 0; j <= max_y; ++j)
+            for (size_t j = 0; j < NUM_Y_ITERATIONS; ++j)
             {
                 double current_bottom_left_y = 0.0 + j * CELL_SIDE_LENGTH;
                 double cell_max_y = current_bottom_left_y + CELL_SIDE_LENGTH;
                 double random_x = get_random_double_in_bounds(current_bottom_left_x, cell_max_x);
                 double random_y = get_random_double_in_bounds(current_bottom_left_y, cell_max_y);
 
-                int increment = mandelbrot_iteration(random_x, random_y);
+                int increment = mandelbrot_iteration(random_x, random_y, MAX_ITERATIONS);
 
                 number_of_cells_inside_mandelbrot_set_th += increment;
                 total_iterations_th += 1;
