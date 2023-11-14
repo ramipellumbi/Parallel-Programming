e2cf7feb6938d6331f54c2924722801efe74d2b3
ramipellumbi <ramipellumbi@gmail.com>
2023-11-14 11:55:31 -0500

    Update task8 run script

--- rp862_ps3_cpsc424/part2/build-run-task8.sh
+++ rp862_ps3_cpsc424/part2/build-run-task8.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 #SBATCH --reservation=cpsc424
 #SBATCH --partition=day
+
+# set total number of MPI processes
+#SBATCH --ntasks=7
+
+# set number of MPI processes per node
+# (number of nodes is calculated by Slurm)
+#SBATCH --ntasks-per-node=2 --ntasks-per-socket=1
+
+# set number of cpus per MPI process
+#SBATCH --cpus-per-task=1
+
+# set memory per cpu
+#SBATCH --mem-per-cpu=7G
 #SBATCH --job-name=T8-MPI_RUN
 #SBATCH --output=out/%x-%j.out
 #SBATCH --time=30:00
-
-# Request 4 nodes in total
-#SBATCH --nodes=4
-# Request 7 tasks in total, which will include 1 manager and 6 workers
-#SBATCH --ntasks=7
-# Request 1 CPU per task
-#SBATCH --cpus-per-task=1
-# Request 7 GB of memory per CPU
-#SBATCH --mem-per-cpu=7G
 
 module purge
 module load iomkl
@@ -33,27 +37,11 @@ make task8
 echo " "
 echo "Task 8"
 
-# Expand the nodelist using scontrol and create a hostfile
-scontrol show hostname $SLURM_JOB_NODELIST > hostfile
-
-# Edit the hostfile to specify the number of slots:
-# The manager node (first line in the hostfile) should have 1 slot.
-# The other nodes should have 2 slots.
-MANAGER_NODE=$(head -n 1 hostfile)
-WORKER_NODES=$(tail -n +2 hostfile)
-
-# Create a new hostfile for mpirun
-echo "$MANAGER_NODE slots=1" > my_hostfile
-for NODE in $WORKER_NODES; do
-    echo "$NODE slots=2" >> my_hostfile
-done
-
 # Run the application with the manager task alone on the first node and two tasks per node on the other nodes
 for k in 1 2 3
 do 
     # Run the MPI program with the custom hostfile
-    # time mpirun --report-bindings -np 7 ./bin/task8
-    time mpirun --report-bindings --hostfile my_hostfile -np 7 ./bin/task8
+    time mpirun --report-bindings -np 7 ./bin/task8
 done
 
 echo "All Done!"
